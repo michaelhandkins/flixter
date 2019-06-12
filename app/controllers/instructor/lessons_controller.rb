203e7f56882a0bede66be0aa0b56e135b2a66e4b
Michael Handkins <michaelhandkins@aol.com>
2019-06-12 21:16:07 +0000

    created lesson permissions so that only the section creator can access the new and create actions for lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,13 +1,13 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
 
   def new
-    @section = Section.find(params[:section_id])
     @lesson = Lesson.new
   end
 
   def create
-    @section = Section.find(params[:section_id])
-    @lesson = @section.lessons.create(lesson_params)
+    @lesson = current_section.lessons.create(lesson_params)
     if @lesson.valid?
       redirect_to instructor_course_path(@section.course)
     else
@@ -21,4 +21,14 @@ class Instructor::LessonsController < ApplicationController
     params.require(:lesson).permit(:title, :subtitle)
   end
 
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
+
+  def require_authorized_for_current_section
+    if current_user != current_section.course.user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
 end
