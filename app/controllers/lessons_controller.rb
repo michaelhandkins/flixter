66a533a49d6c8a87789865c5a476b08f37872e31
Michael Handkins <michaelhandkins@aol.com>
2019-06-18 01:38:42 +0000

    Added permissions to viewing the lessons pages

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,6 @@
 class LessonsController < ApplicationController
-
+before_action :authenticate_user!
+before_action :require_authorized_for_lessons
   def show
 
   end
@@ -9,6 +10,13 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_authorized_for_lessons
+    @course = current_lesson.section.course
+    if current_user.enrolled_in?(@course) == false
+      redirect_to course_path(@course), alert: 'Must be enrolled in course'
+    end
   end
 
 end
