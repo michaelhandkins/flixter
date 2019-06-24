4172d260a8a87c430e857c4b2ba8b1d5c5a3f5d4
Michael Handkins <michaelhandkins@aol.com>
2019-06-24 19:18:12 +0000

    Added ranking/ordering via raning via ranked-model for the lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,9 @@
 class Lesson < ApplicationRecord
   belongs_to :section
   mount_uploader :video, VideoUploader
-
   validates :title, presence: true
+
+  include RankedModel
+
+  ranks :row_order, with_same: :section_id
 end
